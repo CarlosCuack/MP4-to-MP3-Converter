51cd5e4dd6d1beed74d94ab08c04c94f57fdf766
Carlos563429 <carlitosfg.5051@gmail.com>
2023-04-19 21:27:49 -0600

    Update index.py

--- src/index.py
+++ src/index.py
@@ -1,7 +1,21 @@
 import moviepy.editor as mp
+import tkinter as tk
+from tkinter import filedialog
 
-# path = "C:\Users\lenoc\Documents\GitHub\MP4-to-MP3-Converter\src\video.mp4"
+#Variables
 
-mp4 = mp.VideoFileClip(r"C:\Users\lenoc\Documents\GitHub\MP4-to-MP3-Converter\src\video.mp4") #Load the .mp4
 
-mp4.audio.write_audiofile("audio.mp3")
+#Script
+def choosePath():
+    print("HI")
+
+def convertVideo():
+    path = r"C:\Users\lenoc\Documents\GitHub\MP4-to-MP3-Converter\src\video.mp4"
+
+    mp4 = mp.VideoFileClip(path) #Load the .mp4
+
+    mp4.audio.write_audiofile("audio.mp3")
+
+#UI
+
+
